5f585ad13c17509a5e987751cf4441c697fa63d8
Stephen Castaneda <stephen.castaneda40@gmail.com>
2020-02-22 10:15:10 -0600

    added value readers to form

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-console.log("hello")
+//created a function to test submit button and add prevent default to stop auto refresh
+//used getElementById.value to console log values from bootsrap form
+
+const dinos = []
+
+const newDino = (e) => {
+    e.preventDefault();
+    const brandNewDino = {
+        id: `dino${dinos.length + 1}`,
+        name: document.getElementById('dino-name').value,
+        type: document.getElementById('dino-type').value,
+        age: document.getElementById('dino-age').value,
+        owner: document.getElementById('dino-owner').value,
+        adventures: [],
+        health: 100,
+        imageUrl: document.getElementById('dino-image').value
+    };
+    dinos.push(brandNewDino);
+    console.log(dinos);
+
+};
+   
+    // console.log('hi from newDino', dinos);
+
+
+
+//init function to add event listener to submit dino button
+const init = () => {
+    document.getElementById('submit-new-dino').addEventListener('click', newDino);
+   
+};
+
+init();
